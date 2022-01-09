33cfc000ac73abafd609fc297d7fa355e5263ebc
K Waters <waterskm@mail.uc.edu>
2022-01-09 01:48:07 -0500

    instantiated chance and community chest cards

---
+++ Assets/Scripts/Cards.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class Cards
+    {
+        public string CardType { get; set; }
+        public string Message { get; set; }
+        public string Action { get; set; }
+
+        public Cards(string type, string message, string action)
+        {
+            CardType = type;
+            Message = message;
+            Action = action;
+        }
+
+        public static List<Cards> AllCards()
+        {
+            List<Cards> cards = new List<Cards>();
+
+            cards.Add(new Cards(
+                "Chance",
+                "Advance to Illinois Avenue",
+                "move"));
+            cards.Add(new Cards(
+                "Chance",
+                "Pay Poor Tax of $15",
+                "pay"));
+            cards.Add(new Cards(
+                "Chance",
+                "Advance to St. Charles Place",
+                "move"));
+            cards.Add(new Cards(
+                "Chance",
+                "Advance token to nearest utility",
+                "move"));
+            cards.Add(new Cards(
+                "Chance",
+                "Go back 3 spaces",
+                "move"));
+            cards.Add(new Cards(
+                "Chance",
+                "Advance to Go (Collect $200)",
+                "move"));
+            cards.Add(new Cards(
+                "Chance",
+                "Elected Chairman of Board: Pay each player $50",
+                "pay"));
+            cards.Add(new Cards(
+                "Chance",
+                "Make General Repairs. For each house, pay $25. For each hotel, pay $100",
+                "pay"));
+            cards.Add(new Cards(
+                "Chance",
+                "Get Out of Jail Free! This card may be kept until needed or sold",
+                "keep"));
+            cards.Add(new Cards(
+                "Chance",
+                "Advance Token to nearest Railroad and pay owner Twice the rental to which they are owed",
+                "move"));
+            cards.Add(new Cards(
+                "Chance",
+                "Take a ride on the Reading Railroad. If you pass go, collect $200",
+                "move"));
+            cards.Add(new Cards(
+                "Chance",
+                "Building and Loan Matures, Collect $150",
+                "collect"));
+            cards.Add(new Cards(
+                "Chance",
+                "Advance Token to Nearest Railroad and pay owner Twice the rental to which they are owed",
+                "collect"));
+            cards.Add(new Cards(
+                "Chance",
+                "Advance token to Boardwalk",
+                "move"));
+            cards.Add(new Cards(
+                "Chance", "Bank pays you dividend of $50",
+                "collect"));
+            cards.Add(new Cards(
+                "Chance",
+                "Go Directly to Jail. Do not pass Go, do not collect $200",
+                "move"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Life Insurance Matures. Collect $100",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Income Tax Refund, collect $20",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Receive for services $25",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Pay School Tax of $150",
+                "pay"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Street Repairs. $40 per house, $115 per hotel",
+                "pay"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Xmas fund matures, collect $100",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Doctor's Fee, pay $50",
+                "pay"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Get Out of Jail Free. This card may be kept until needed or sold",
+                "keep"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Sale of Stock, you get $45",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Advance to Go, collect $200",
+                "move"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Inherit $100",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Second Prize in a Beauty Contest, collect $10",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Bank Error in your Favor, collect $200",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Go Directly to Jail. Do not pass Go, do not collect $200",
+                "move"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Grand Opera Opening: Collect $50 from everyone",
+                "collect"));
+            cards.Add(new Cards(
+                "Community Chest",
+                "Pay Hospital $100",
+                "pay"));
+
+            return cards;
+        }
+    
+        public static List<Cards> CreateDeck_Chance()
+        {
+            List<Cards> chanceDeck = new List<Cards>();
+            foreach (var card in AllCards())
+            {
+                if (card.CardType.Equals("Chance"))
+                {
+                    chanceDeck.Add(card);
+                }
+            }
+            return chanceDeck;
+        }
+
+        public static List<Cards> CreateDeck_CommunityChest()
+        {
+            List<Cards> communityChestDeck = new List<Cards>();
+            foreach (var card in AllCards())
+            {
+                if (card.CardType.Equals("Community Chest"))
+                {
+                    communityChestDeck.Add(card);
+                }
+            }
+            return communityChestDeck;
+        }
+
+
+    }
+}
--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
-    private Sprite[] diceSides1;
-    //private Sprite[] diceSides2;
-    private SpriteRenderer rend1;
-    //private SpriteRenderer rend2;
-    private int whosTurn = 1;
+    public static Sprite[] diceSides1;
+    public static Sprite[] diceSides2;
+    public static SpriteRenderer rend1;
+    public static SpriteRenderer rend2;
+    public static int whosTurn = 1;
     private bool coroutineAllowed = true;
+    public static bool isDoubles = false;
+    public static int doublesCounter = 0;
 
     // Start is called before the first frame update
     private void Start()
@@ -17,12 +19,6 @@ public class Dice : MonoBehaviour
         rend1 = GetComponent<SpriteRenderer>();
         diceSides1 = Resources.LoadAll<Sprite>("DiceSides/");
         rend1.sprite = diceSides1[diceSides1.Length - 1];
-
-        /*
-        rend2 = GetComponent<SpriteRenderer>();
-        diceSides2 = Resources.LoadAll<Sprite>("Art/Dice");
-        rend2.sprite = diceSides2[5];
-        */
     }
 
     private void OnMouseDown()
@@ -33,21 +29,55 @@ public class Dice : MonoBehaviour
         }
     }
 
+    public int StoreRoll (int roll)
+    {
+        return roll;
+    }
+
     private IEnumerator RollTheDice()
     {
         coroutineAllowed = false;
         int randomDiceSide1 = 0;
-        //int randomDiceSide2 = 0;
-        for (int i = 0; i <= 20; i++)
+        int firstRoll = 0;
+        int secondRoll = 0;
+        int totalRoll = 0;
+        int rolledValue = 0;
+        for (int x = 0; x <=1; x++)
         {
-            randomDiceSide1 = Random.Range(0, 6);
-            //randomDiceSide2 = Random.Range(0, 6);
-            rend1.sprite = diceSides1[randomDiceSide1];
-            //rend2.sprite = diceSides2[randomDiceSide2];
-            yield return new WaitForSeconds(0.05f);
+            for (int i = 0; i <= 20; i++)
+            {
+                randomDiceSide1 = Random.Range(0, 6);
+                rend1.sprite = diceSides1[randomDiceSide1];
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            rolledValue = randomDiceSide1 + 1;
+            if(x == 0)
+            {
+                firstRoll = StoreRoll(rolledValue);
+                Debug.Log("First roll: " + firstRoll);
+            }
+            else
+            {
+                secondRoll = StoreRoll(rolledValue);
+                Debug.Log("Second roll: " + secondRoll);
+            }
+            Debug.Log("rolled a " + rolledValue);
+            totalRoll = totalRoll + rolledValue;
+            Debug.Log("in total: " + totalRoll);
+            randomDiceSide1 = 0;
         }
 
-        GameControl.diceSideThrown = (randomDiceSide1) + 1;
+        if (firstRoll == secondRoll)
+        {
+            isDoubles = true;
+            doublesCounter++;
+            Debug.Log("Did you roll doubles to get a " + totalRoll + "? " + isDoubles);
+            Debug.Log("How many doubles did you roll? " + doublesCounter);
+        }
+        isDoubles = false;
+
+        GameControl.diceSideThrown = totalRoll;
         if (whosTurn == 1)
         {
             GameControl.MovePlayer(1);
@@ -56,6 +86,8 @@ public class Dice : MonoBehaviour
             GameControl.MovePlayer(2);
         }
         whosTurn *= -1;
+        isDoubles = false;
+        doublesCounter = 0;
         coroutineAllowed = true;
     }
 }
